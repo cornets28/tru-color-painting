12888f9ee28214c61a7c6c9009c98f14b7b9f0f5
CornetS28 <corsam28@gmail.com>
2020-07-05 22:41:25 -0400

    delete useless code in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello from app</h1>
+
+      <div className="row">
+        <div className="col-6">number 1</div>
+        <div className="col-6">
+          <span>
+            <i className="fas fa-home" />
+          </span>
+        </div>
+      </div>
+
+      {/* <div class="preloader">
+        <div class="p-wrapper">
+        <div class="spinner">
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+        </div>
+        </div>
+      </div> */}
     </div>
   );
 }
