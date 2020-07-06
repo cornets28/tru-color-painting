09c5a80a1cd07a57c794d0b02305ed9b7ca3bd33
CornetS28 <corsam28@gmail.com>
2020-07-05 23:20:14 -0400

    create basic component files

---
+++ src/component/AboutUs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from about us</h3>
+      </div>
+    );
+  }
+}
---
+++ src/component/HouseDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HouseDetails extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from House Details</h3>
+      </div>
+    );
+  }
+}
