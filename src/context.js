eea2c93d3f5693a60e61b625e99a2f9455a73d96
CornetS28 <corsam28@gmail.com>
2020-07-12 16:02:30 -0400

    create provider nd consumer to retrieve the data

--- src/context.js
+++ src/context.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { paintingHouses } from './data';
 
 const PaintingContext = React.createContext()
 
-class PaitingProvider extends Component {
+class PaintingProvider extends Component {
+  state = {
+    paintings: paintingHouses
+  };
   render() {
     return (
-      <PaintingContext.Provider value="Hello form context">
+      <PaintingContext.Provider value={{...this.state}}>
         {this.props.children}
       </PaintingContext.Provider>
-    )
+    );
   }
 }
 const PaintingConsumer = PaintingContext.Consumer;
 
-export default( PaitingProvider, PaintingConsumer)
+export { PaintingProvider, PaintingConsumer};
