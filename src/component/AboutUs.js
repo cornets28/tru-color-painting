ca540b9cf13a577d0b91aa0810b1a3c035fc02a1
CornetS28 <corsam28@gmail.com>
2020-07-15 01:47:16 -0400

    interate over the data array to get the item from the consumer

--- src/component/AboutUs.js
+++ src/component/AboutUs.js
@@ -1,139 +1,90 @@
 import React, { Component } from "react";
+import { PaintingConsumer } from "../context";
+import Carousel from "./Carousel";
+import { carouselPhotos } from "../data";
 
 export default class AboutUs extends Component {
+  state = {
+    carouselPics: carouselPhotos,
+  };
   render() {
     return (
-      <section class="client-feedback">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-5">
-              <div class="client-image wow fadeInLeft" data-wow-delay=".5s">
-                <img src="/images/client.png" alt="" />
+      <React.Fragment>
+        <section class="client-feedback">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-5">
+                <div class="client-image wow fadeInLeft" data-wow-delay=".5s">
+                  <img src="/images/client.png" alt="" />
+                </div>
               </div>
-            </div>
-            <div class="col-md-7">
-              <div class="client-content">
-                <p>We Love What We Do</p>
-                <h3 data-aos="zoom-in">We Can Paint You Dream House</h3>
-                <p>
-                  Fall In Love With Your House By Choosing Us. Highest quality
-                  painting and most professional services.
-                </p>
-                <div class="testimo-slide">
-                  <div
-                    class="carousel slide"
-                    data-ride="carousel"
-                    id="quote-carousel"
-                  >
-                    <div class="carousel-inner">
-                      <div class="item active carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img
-                              src="/images/house-1.jpg"
-                              alt="CarouselImage"
-                            />
+              <div class="col-md-7">
+                <div class="client-content">
+                  <p>We Love What We Do</p>
+                  <h3 data-aos="zoom-in">We Can Paint You Dream House</h3>
+                  <p>
+                    Fall In Love With Your House By Choosing Us. Highest quality
+                    painting and most professional services.
+                  </p>
+                  <div class="testimo-slide">
+                    <div
+                      class="carousel slide"
+                      data-ride="carousel"
+                      id="quote-carousel"
+                    >
+                      <div class="carousel-inner">
+                        <div class="item active carousel-item">
+                          <div class="row">
+                            <div class="col-md-12">
+                              <img
+                                src="/images/house-1.jpg"
+                                alt="CarouselImage"
+                              />
+                            </div>
                           </div>
                         </div>
+
+                        
+                          <PaintingConsumer>
+                            {(value) => {
+                              return value.carouselPics.map((carousPhoto) => {
+                                return (
+                                  <Carousel
+                                    key={carousPhoto.id}
+                                    carousPhoto={carousPhoto}
+                                  />
+                                );
+                              });
+                            }}
+                          </PaintingConsumer>
+                        
                       </div>
 
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-2.jpg" alt="" />
-                          </div>
-                        </div>
+                      <div class="testimonial-nav prev-next">
+                        <a
+                          data-slide="prev"
+                          href="#quote-carousel"
+                          class="left carousel-control btn-prev"
+                        >
+                          <i class="fa fa-chevron-left"></i>
+                        </a>
+                        <a
+                          data-slide="next"
+                          href="#quote-carousel"
+                          class="right carousel-control btn-next"
+                        >
+                          <i class="fa fa-chevron-right"></i>
+                        </a>
                       </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-3.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-4.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-5.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-10.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-9.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-8.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-7.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="item carousel-item">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <img src="/images/house-6.jpg" alt="" />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                   
-                    <div class="testimonial-nav prev-next">
-                      <a
-                        data-slide="prev"
-                        href="#quote-carousel"
-                        class="left carousel-control btn-prev"
-                      >
-                        <i class="fa fa-chevron-left"></i>
-                      </a>
-                      <a
-                        data-slide="next"
-                        href="#quote-carousel"
-                        class="right carousel-control btn-next"
-                      >
-                        <i class="fa fa-chevron-right"></i>
-                      </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </React.Fragment>
     );
   }
 }
+
