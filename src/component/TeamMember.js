871f0b2b3de3585a6a94e8fb58d63172a8766f32
CornetS28 <corsam28@gmail.com>
2020-07-15 21:20:16 -0400

    create team member component

---
+++ src/component/TeamMember.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-expressions */
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+
+     
+export default class TeamMember extends Component {
+  
+  render() {
+    const {id, firstname, lastname, photo, position, facebookLink } = this.props.member
+    
+    return (
+      <div className="col-md-4 wow fadeInUp" data-wow-delay=".5s">
+        <div className="st-member">
+          <img src={photo} alt="" />
+          <div className="st-s-icon">
+            <ul>
+              <li>
+                {/* <Link to={()=>this.props.history.push(facebookLink)}> */}
+                <i
+                  onClick={(event) => (window.location.href = facebookLink)}
+                  className="fab fa-facebook-f sicon sticon"
+                ></i>
+                {/* </Link> */}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <h3 className="fullname wow fadeInDown" data-wow-delay="1s">
+          {firstname + " " + lastname}
+        </h3>
+        <p className="wow fadeInDown" data-wow-delay="1.5s">
+          {position}
+        </p>
+      </div>
+    );
+  }
+}
