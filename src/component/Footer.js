2775a82850ea8c1ad4d9b148ea473ad904903a83
CornetS28 <corsam28@gmail.com>
2020-07-19 22:58:40 -0400

    fix linter errors

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import PaintingForm from './PaintingForm';
+// import PaintingForm from './PaintingForm';
 
 export default class Footer extends Component {
   render() {
